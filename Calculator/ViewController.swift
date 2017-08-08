c2fe1e0ce2fcc94e0d1c3e9fe8238adec0870206
mnyk <nykielmat@gmail.com>
2017-08-08 15:55:58 +0200

    calc v 2.0
    another version:
    1. without the special buttons,
    2. with the basic usability.

--- Calculator/ViewController.swift
+++ Calculator/ViewController.swift
@@ -10,44 +10,22 @@
 import UIKit
 
 class ViewController: UIViewController {
-    
-    @IBOutlet weak var display: UILabel!
+   //display
+    @IBOutlet weak var display: UILabel?
     
     var userIsInTheMiddleOfTyping = false
     
+  //buttons
     @IBAction func touchDigit(_ sender: UIButton) {
         let digit = sender.currentTitle!
         if userIsInTheMiddleOfTyping {
-            let textCurrentlyInDisplay = display.text!
-            display.text = textCurrentlyInDisplay + digit
+        let textCurrentlyInDisplay = display!.text!
+        display!.text = textCurrentlyInDisplay + digit
         } else {
-            display.text = digit
+            display!.text = digit
             userIsInTheMiddleOfTyping = true
         }
     }
     
-    var displayValue: Double {
-        get {
-            return Double(display.text!)!
-        }
-        set {
-            display.text = String(newValue)
-        }
-    }
-    private var brain = CalculatorBrain()
     
-    @IBAction func performOperation(_ sender: UIButton) {
-        if userIsInTheMiddleOfTyping{
-            brain.setOperand(displayValue)
-            userIsInTheMiddleOfTyping = false
-        }
-        if let mathematicalSymbol = sender.currentTitle  {
-            brain.performOperation(mathematicalSymbol)
-                       }
-        if let result = brain.result{
-        displayValue = result
-        }
-    }
-    }
-
-
+}
