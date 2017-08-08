e3a82461b4eb3862b18a90fa206be325ac296900
mnyk <nykielmat@gmail.com>
2017-08-08 16:21:07 +0200

    switch-case
    simply switch-case instruction in viewController with the two cases

--- Calculator/ViewController.swift
+++ Calculator/ViewController.swift
@@ -10,22 +10,38 @@
 import UIKit
 
 class ViewController: UIViewController {
-   //display
+    //display
     @IBOutlet weak var display: UILabel?
     
     var userIsInTheMiddleOfTyping = false
     
-  //buttons
+    //buttons
     @IBAction func touchDigit(_ sender: UIButton) {
         let digit = sender.currentTitle!
         if userIsInTheMiddleOfTyping {
-        let textCurrentlyInDisplay = display!.text!
-        display!.text = textCurrentlyInDisplay + digit
+            let textCurrentlyInDisplay = display!.text!
+            display!.text = textCurrentlyInDisplay + digit
         } else {
             display!.text = digit
             userIsInTheMiddleOfTyping = true
         }
     }
     
+    
+    @IBAction func performOperation(_ sender: UIButton) {
+        userIsInTheMiddleOfTyping = false
+        if let mathematicalSymbol = sender.currentTitle{
+            switch mathematicalSymbol {
+            case "pi": display!.text = "3.1415926"
+            case "√" :
+                let operand = Double(display!.text!)!
+                display!.text = String(sqrt(operand))
+            default:
+                break
+            }
+            
+            
+        }
+    }
     
 }
