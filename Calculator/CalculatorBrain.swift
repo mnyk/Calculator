c9dd1843c2d163f62049b4bbb1bc8e6478cabac7
mnyk <nykielmat@gmail.com>
2017-08-08 17:30:31 +0200

    different operations
    three new different op. with dictionary structure

--- Calculator/CalculatorBrain.swift
+++ Calculator/CalculatorBrain.swift
@@ -12,16 +12,44 @@ struct CalculatorBrain {
     
     private var accumulator: Double?
     
+    private enum Operation {
+        case constant(Double)
+        case unaryOperation
+        case binaryOperation
+    }
+    
+    private var operations: Dictionary<String,Operation> = [
+        "pi"    :   Operation.constant(Double.pi), //Double.pi,
+        "e"     :   Operation.constant(M_E), //M_E,
+        "√"     :   Operation.unaryOperation, //sqrt,
+        "cos"   :   Operation.unaryOperation, //cos,
+    
+    ]
+    
     mutating func performOperation(_ symbol: String) {
+        
+        if let operation = operations[symbol]{
+            switch operation {
+            case .constant(let value):
+                accumulator = value
+            case .unaryOperation:
+                break
+            case .binaryOperation:
+                break
+            }
+        }
+        /*
         switch symbol {
         case "pi": accumulator = Double.pi
         case "√" :
             if let operand = accumulator {
             accumulator = sqrt(operand)
             }
+            
         default:
             break
-        }
+         */
+        
     }
         
     
